2c421a065107b41eaebfb5843fa82e6203b67d0d
rahulreddyp <rahulreddyp77@gmail.com>
2020-12-30 16:57:05 +0530

    fix: changed cpyright date and project images

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ $('.navbar-collapse a').click(function() {
     $("navbar-collapse").collapse('hide');
 });
 
-
+// Clear the form data and reset
 function clearForm() {
     document.getElementById("contactUs").reset(); 
 }
+
+// Dynamically changing current year for copyright footer
+var copyrightYear = document.querySelector(".copyright-year");
+var date = new Date();
+
+copyrightYear.textContent = date.getFullYear();
